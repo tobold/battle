f160dab6640bc9b50f5a24f9cd550c4378fa58c2
Ryan Davidson <daviryan@aol.com>
2017-07-18 13:29:51 +0100

    Add features/feature_test.rb to test infrastructure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,16 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  ENV['RACK_ENV'] = 'test'
+
+  require_relative '../app.rb'
+
+  require 'capybara'
+  require 'capybara/rspec'
+  require 'rspec'
+
+  Capybara.app = Battle
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.

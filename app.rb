3500f78fd6362bf4b2a95980fafb2c4e1bf0be35
Toby Hamand <tobyhamand@gmail.com>
2017-07-18 15:55:00 +0100

    added push redirect get pattern

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require 'sinatra'
 
 class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret' #if using Shotgun
+
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+  post '/names' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 end
